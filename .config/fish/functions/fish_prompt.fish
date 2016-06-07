2267f139c5f41fd46661cd3c6db4836b39e2ab4c
Terence Tuhinanshu <ttuhinanshu@azavea.com>
2016-06-07 11:40:51 -0400

    Add fish prompt

---
+++ .config/fish/functions/fish_prompt.fish
@@ -0,0 +1,28 @@
+function __parse_git_branch
+    git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
+end
+
+function fish_prompt --description 'Write out the prompt'
+	# Just calculate this once, to save a few cycles when displaying the prompt
+	if not set -q __fish_prompt_hostname
+		set -g __fish_prompt_hostname (hostname|cut -d . -f 1)
+	end
+
+	set -l color_cwd
+	set -l suffix
+	switch $USER
+	case root toor
+		if set -q fish_color_cwd_root
+			set color_cwd $fish_color_cwd_root
+		else
+			set color_cwd yellow
+		end
+		set suffix '#'
+	case '*'
+		set color_cwd yellow
+		set suffix '>'
+	end
+
+	echo -s (set_color green) "$USER" @ "$__fish_prompt_hostname" ' ' (set_color $color_cwd) (prompt_pwd) (set_color red) (__parse_git_branch) (set_color normal)
+	echo "$suffix "
+end
