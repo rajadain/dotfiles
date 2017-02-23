0398bc1b76c8513d0e07e2fbc308f3ad2885deaa
Terence Tuhinanshu <ttuhinanshu@azavea.com>
2017-02-23 11:32:43 -0500

    Update vkj to be more generalized
    While all scripts on #civicapps end with `.sh`, other teams
    have scripts without extensions. Thus update allows the use
    of scripts with and without extensions.
    
    This also quotes all parameters individually now, allowing
    the use of quoted parameters such as:
    
        vkj console django './manage.py migrate'

--- .config/fish/functions/vkj.fish
+++ .config/fish/functions/vkj.fish
@@ -1,20 +1,26 @@
 #!/usr/bin/fish
-function vkj -d "Run scripts from inside vagrant" 
+function vkj -d "Run scripts from inside vagrant"
     if test (count $argv) -lt 1
         echo "vkj runs scripts inside vagrant."
         echo "The script must exist in /vagrant/scripts/ inside the VM,"
         echo "and the VM must be up and running."
     else
-        set -l script ./scripts/$argv[1].sh
-        if test -f $script
-            set -l sargs
-            if test (count $argv) -ge 2
-                set sargs $argv[2..-1]
+        set -l script ./scripts/$argv[1]
+        if not test -f $script
+            set script $script.sh
+            if not test -f $script
+                echo "Could not find script $script"
+                exit 1
             end
-
-            vagrant ssh -c "cd /vagrant && $script $sargs"
-        else
-            echo "Could not find script $script"
         end
+
+        set -l sargs
+        if test (count $argv) -ge 2
+            for arg in $argv[2..-1]
+                set sargs $sargs \'$arg\'
+            end
+        end
+
+        vagrant ssh -c "cd /vagrant && $script $sargs"
     end
 end
