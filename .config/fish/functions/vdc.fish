de2b655ac7cbef7c7f58a8d04baccc6c2c9a33a6
Terence Tuhinanshu <ttuhinanshu@azavea.com>
2017-03-03 15:15:05 -0500

    Support quoted parameters in vdc as well
    Just like for vkj in the last commit.

--- .config/fish/functions/vdc.fish
+++ .config/fish/functions/vdc.fish
@@ -3,6 +3,13 @@ function vdc -d "Runs docker-compose from inside vagrant"
     if test (count $argv) -lt 1
         echo "vdc runs docker-compose from inside vagrant."
     else
-        vagrant ssh -c "cd /vagrant && docker-compose $argv"
+        set -l sargs
+        if test (count $argv) -ge 1
+            for arg in $argv[1..-1]
+                set sargs $sargs \'$arg\'
+            end
+        end
+
+        vagrant ssh -c "cd /vagrant && docker-compose $sargs"
     end
 end
