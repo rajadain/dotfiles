381258af7d7aa53a5b607fff614486760c488944
Terence Tuhinanshu <terence@tuhinanshu.com>
2020-02-21 20:31:31 -0500

    Add z, pure prompt, and its configuration
    Also add fishfile and fish_variables to the repo. These will
    have to by symlinked separately.

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,42 +1,11 @@
-function __parse_git_branch
-    git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'
-end
+function fish_prompt
+    set --local exit_code $status  # save previous exit code
 
-function fish_prompt --description 'Write out the prompt'
-    # Just calculate this once, to save a few cycles when displaying the prompt
-    if not set -q __fish_prompt_hostname
-        set -g __fish_prompt_hostname (hostname|cut -d . -f 1)
-    end
+    echo -e -n (_pure_prompt_beginning)  # init prompt context (clear current line, etc.)
+    echo -e (_pure_prompt_first_line)  # print current path, git branch/status, command duration
+    _pure_place_iterm2_prompt_mark # place iTerm shell integration mark
+    echo -e -n (_pure_prompt $exit_code)  # print prompt
+    echo -e (_pure_prompt_ending)  # reset colors and end prompt
 
-    set -l color_cwd
-    set -l suffix
-    switch $USER
-    case root toor
-        if set -q fish_color_cwd_root
-            set color_cwd $fish_color_cwd_root
-        else
-            set color_cwd yellow
-        end
-        set suffix '#'
-    case '*'
-        set color_cwd yellow
-        set suffix '>'
-    end
-
-    set -l prompt_pwd (prompt_pwd)
-    set -l git_branch (__parse_git_branch)
-
-    set -l left_prompt "$USER@$__fish_prompt_hostname $prompt_pwd $git_branch"
-    set -l left_prompt_color (set_color green)"$USER@$__fish_prompt_hostname" (set_color yellow)$prompt_pwd (set_color red)$git_branch
-
-    set -l right_prompt (date "+%y-%m-%d %H:%M:%S")
-    set -l right_prompt_color (set_color normal)$right_prompt
-
-    set -l columns (math $COLUMNS - (string length $left_prompt) - (string length $right_prompt) + 27)
-    if not test -n "$git_branch"
-        set columns (math $columns + 1)
-    end
-
-    printf "%s %"$columns"s\n" "$left_prompt_color" "$right_prompt_color"
-    echo -s (set_color normal) "$suffix "
+    set _pure_fresh_session false
 end
