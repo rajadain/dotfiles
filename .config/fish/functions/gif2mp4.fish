b6dd233746d87a9a565a99250fb7098e2b2e4e5f
Terence Tuhinanshu <terence@tuhinanshu.com>
2022-11-08 15:25:16 -0500

    Improve gif2mp4 with automatic flags
    This will automatically pick the most recent gif, and delete the
    gif after deletion.

--- .config/fish/functions/gif2mp4.fish
+++ .config/fish/functions/gif2mp4.fish
@@ -1,5 +1,5 @@
 #!/usr/bin/fish
-function gif2mp4 -d "Converts GIF files to MP4 with ffmpeg"
+function _gif2mp4
 	if test (count $argv) -lt 1
         echo "gif2mp4 converts GIF files to MP4 with ffmpeg"
     else
@@ -12,4 +12,23 @@ function gif2mp4 -d "Converts GIF files to MP4 with ffmpeg"
     		end
     	end
     end
+end
+
+function gif2mp4 -d "Converts GIF files to MP4 with ffmpeg"
+	argparse 'd/delete' 'a/autolast' -- $argv
+
+	# --delete will delete the original .gif after successful conversion
+	# --autolast will pick the alphabetically most recent .gif
+
+	if set --query _flag_autolast
+		set -l MOST_RECENT_GIF (ls *.gif | tail -1)
+		
+		if _gif2mp4 "$MOST_RECENT_GIF"; and set --query _flag_delete
+			rm "$MOST_RECENT_GIF"
+		end
+	else
+		if _gif2mp4 $argv; and set --query _flag_delete
+			rm $argv
+		end
+	end
 end
