3bc6448e35c228d836f0f07be099c022f35fee9d
Terence Tuhinanshu <terence@tuhinanshu.com>
2026-01-20 10:53:03 -0500

    Catching up on neshaminy updates

---
+++ .config/fish/functions/br.fish
@@ -0,0 +1,1 @@
+/Users/ttuhinanshu/Library/Application Support/org.dystroy.broot/launcher/fish/br.fish
---
+++ .config/fish/functions/vsc.fish
@@ -0,0 +1,3 @@
+function vsc --wraps="open -a 'Visual Studio Code'" --description "alias vsc open -a 'Visual Studio Code'"
+  open -a 'Visual Studio Code' $argv
+end
