aa8428d73dac1c8c6199d7a02bae3dc2551e90a5
Terence Tuhinanshu <terence@tuhinanshu.com>
2026-02-04 13:20:39 -0500

    More optimization

--- .config/fish/conf.d/venv.fish
+++ .config/fish/conf.d/venv.fish
@@ -11,15 +11,19 @@
 
 # where to look for virtual environments
 function __venv_base
-  # Optimization: Check if .git exists in the current dir first (fastest)
-  if test -d .git
-    pwd
-    # Otherwise, check if we are in a git sub-directory
-  else if command git rev-parse --is-inside-work-tree >/dev/null 2>&1
-    command git rev-parse --show-toplevel 2>/dev/null
-  else
-    pwd
+  # If we are in the home directory, stop immediately. 0ms cost.
+  if test "$PWD" = "$HOME"
+    return 1
   end
+
+  # Fast check: are we at the root of a git repo?
+  if test -d .git
+    echo "$PWD"
+    return 0
+  end
+
+  # Only now do we call the git binary
+  command git rev-parse --show-toplevel 2>/dev/null
 end
 
 # find the virtualenv, whatever it is called
@@ -49,9 +53,20 @@ function __handle_venv_activation --argument-names dir
   end
 end
 
-function __auto_source_venv --on-variable PWD --description "Activate/Deactivate virtualenv on directory change"
+function __auto_source_venv --on-variable PWD --description "Activate/Deactivate virtualenv"
   status --is-command-substitution; and return
-  __handle_venv_activation (__venv_base)
+
+  # Optimization: If we are already in a Venv and the PWD is still inside 
+  # that Venv's parent, do nothing.
+  if set -q VIRTUAL_ENV
+    set -l venv_parent (dirname "$VIRTUAL_ENV")
+    if string match -q "$venv_parent*" "$PWD"
+      return
+    end
+  end
+
+  set -l target_dir (__venv_base)
+  __handle_venv_activation "$target_dir"
 end
 
 # Initial call on shell start
