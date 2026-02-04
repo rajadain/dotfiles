38f17bd6ad3bfa1b00b997c22f77196fc05abd73
Terence Tuhinanshu <terence@tuhinanshu.com>
2026-02-04 13:14:11 -0500

    Optimize venv with Gemini's help

--- .config/fish/conf.d/venv.fish
+++ .config/fish/conf.d/venv.fish
@@ -7,16 +7,26 @@
 #   for other means of cd, such as z.
 # * Update syntax to work with new versions of fish.
 
+# Optimized Venv Auto-activation
+
 # where to look for virtual environments
 function __venv_base
-  git rev-parse --show-toplevel 2>/dev/null; or pwd
+  # Optimization: Check if .git exists in the current dir first (fastest)
+  if test -d .git
+    pwd
+    # Otherwise, check if we are in a git sub-directory
+  else if command git rev-parse --is-inside-work-tree >/dev/null 2>&1
+    command git rev-parse --show-toplevel 2>/dev/null
+  else
+    pwd
+  end
 end
 
 # find the virtualenv, whatever it is called
 function __venv --argument-names dir
-  set VENV_DIR_NAMES env .env venv .venv
+  set -l VENV_DIR_NAMES env .env venv .venv
   for venv_dir in $dir/$VENV_DIR_NAMES
-    if test -e "$venv_dir/bin/activate.fish"
+    if test -f "$venv_dir/bin/activate.fish"
       echo "$venv_dir"
       return
     end
@@ -25,9 +35,12 @@ function __venv --argument-names dir
 end
 
 function __handle_venv_activation --argument-names dir
-  set -l venv_dir (__venv $dir); or begin
-    # no virtual env found, deactivate any existing virtual env 
-    set -q VIRTUAL_ENV; and deactivate
+  set -l venv_dir (__venv $dir)
+  if test $status -ne 0
+    # no virtual env found, deactivate any existing virtual env
+    if set -q VIRTUAL_ENV
+      deactivate
+    end
     return
   end
 
@@ -41,4 +54,5 @@ function __auto_source_venv --on-variable PWD --description "Activate/Deactivate
   __handle_venv_activation (__venv_base)
 end
 
+# Initial call on shell start
 __auto_source_venv
